3f510a4824e0977ff0580dbb45426825f93de2ef
Freya Gaynor <freya@webwitch.org>
2023-06-25 11:22:37 -0500

    feature: Initialize project.

---
+++ mod.cpp
@@ -0,0 +1,17 @@
+#define ICON "logo_ca.paa"
+
+// https://community.bistudio.com/wiki/Mod.cpp/bin_File_Format
+name = "SWS Addons";
+author = "Maid";
+logo = ICON;
+logoOver = ICON;
+tooltip = "Scarlet Wolf Sorority Addons";
+tooltipOwned = "SWS Addons Owned";
+picture = ICON;
+actionName = "GitHub";
+action = "https://github.com/WitchFreya/SWSAddons";
+overview = "Addons for SWS unit content.";
+overviewPicture = ICON;
+overviewText = "Addons for SWS unit content.";
+overviewFootnote = "<br/><br/><t color='#999999'>This content is under the APL-SA License.</t>";
+description = "Addons for SWS unit content.";
