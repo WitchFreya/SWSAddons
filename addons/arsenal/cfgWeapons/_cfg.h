2950129ef9bfbbefe637db3fd7c75b67901e9053
Freya Gaynor <freya@webwitch.org>
2023-06-25 17:38:36 -0500

    feature: Initial content.

---
+++ addons/arsenal/cfgWeapons/_cfg.h
@@ -0,0 +1,7 @@
+class ItemInfo;
+
+#include "dependencies\optre.h"
+#include "base.h"
+#include "tiger.h"
+#include "polaris.h"
+#include "egg.h"
---
+++ addons/arsenal/cfgWeapons/polaris.h
@@ -0,0 +1,22 @@
+class SWS_Vest_Polaris : SWS_Base_Armor
+{
+    dlc = "sws";
+    author = "Tiger";
+    displayName = "[SWS] Polaris' Armor";
+    hiddenSelectionsTextures[] = {
+        "11th_stb_gear\_textures\misc\Armor\V\vest_odst_co.paa",
+        "11th_stb_gear\_textures\misc\Armor\A\armor_odst_co.paa",
+        "11th_stb_gear\_textures\11th\Hades_L\Hades_1-1_Legs.paa",
+        "optre_unsc_units\army\data\ghillie_desert_co.paa",
+        TEXTURE(armor_polaris.paa)};
+
+    class ItemInfo : ItemInfo
+    {
+        hiddenSelectionsTextures[] = {
+            "11th_stb_gear\_textures\misc\Armor\V\vest_odst_co.paa",
+            "11th_stb_gear\_textures\misc\Armor\A\armor_odst_co.paa",
+            "11th_stb_gear\_textures\11th\Hades_L\Hades_1-1_Legs.paa",
+            "optre_unsc_units\army\data\ghillie_desert_co.paa",
+            TEXTURE(armor_polaris.paa)};
+    };
+};
