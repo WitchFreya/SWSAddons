8316da2cc4888fc87f2c1f2648f8e824855d584d
Freya Gaynor <freya@webwitch.org>
2023-06-29 12:33:14 -0500

    feature: Fixed a few things.

--- addons/arsenal/gear/operatives/polaris.hpp
+++ addons/arsenal/gear/operatives/polaris.hpp
@@ -1,11 +1,11 @@
-
-HELMET(Butler, TEXTURE(helmet_butler_co.paa),
-       TEXTURE(visr_purple_co.paa),
+HELMET(Polaris, TEXTURE(helmet_polaris_co.paa),
+       TEXTURE(visr_pink_co.paa),
        "optre_unsc_units\army\data\ghillie_woodland_co.paa",
        "optre_unsc_units\army\data\soft_packs_co.paa");
 
-VEST(Butler, TEXTURE(vest_rifleman_co.paa),
+VEST(Polaris,
+     TEXTURE(vest_rifleman_co.paa),
      TEXTURE(armor_rifleman_co.paa),
-     TEXTURE(legs_butler_co.paa),
+     TEXTURE(legs_polaris_co.paa),
      "optre_unsc_units\army\data\ghillie_desert_co.paa",
-     TEXTURE(odst_addons_butler_co.paa));
+     TEXTURE(odst_addons_polaris_co.paa));
