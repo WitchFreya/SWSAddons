7063cafc216463b19c8abc23708d5f4aa426ed82
Freya Gaynor <freya@webwitch.org>
2023-07-04 13:30:10 -0500

    feature: Categories for AR-0M37

--- addons/weapons/CfgWeapons.hpp
+++ addons/weapons/CfgWeapons.hpp
@@ -1,30 +1,37 @@
-#define WEAPON(var0) TRIPLES(SWS,Weapon,var0)
-#define WEAPON_CLASSES          \
-    QUOTE(WEAPON(AR0M37)),      \
-    QUOTE(WEAPON(AR0M37_GL)),   \
-    QUOTE(WEAPON(AR0M37_SHT))
-#define DISPLAY_NAME(var0) QUOTE([SWS] var0)
-#define WEAPON_META(var0,scope) \
-    ITEM_META(scope);           \
-    baseweapon = QUOTE(WEAPON(var0))
-
+#define WEAPON_CLASSES  \
+    QWEAPON(AR0M37),    \
+    QWEAPON(AR0M37_GL), \
+    QWEAPON(AR0M37_SHT)
 
 class CfgWeapons {
     #include "dependencies\arma.hpp"
     #include "dependencies\opcan.hpp"
     class WEAPON(AR0M37): LM_OPCAN_AR0M37 {
         WEAPON_META(AR0M37,2);
-        displayName = DISPLAY_NAME(AR-0M37 6.5mm);
+        displayName = NAME(AR-0M37 6.5mm);
         picture = QPATHTOF(data\icons\msbs65_ca.paa);
+
+        class XtdGearInfo {
+            model = QWEAPON(AR0M37);
+            underbarrel = "None";
+        };
     };
     class WEAPON(AR0M37_GL): LM_OPCAN_AR0M37_GL {
         WEAPON_META(AR0M37_GL,2);
-        displayName = DISPLAY_NAME(AR-0M37 GL 6.5mm);
+        displayName = NAME(AR-0M37 GL 6.5mm);
         picture = QPATHTOF(data\icons\msbs65_barrel_ca.paa);
+        class XtdGearInfo {
+            model = QWEAPON(AR0M37);
+            underbarrel = "GL";
+        };
     };
     class WEAPON(AR0M37_SHT): LM_OPCAN_AR0M37_SHT {
         WEAPON_META(AR0M37_SHT,2);
-        displayName = DISPLAY_NAME(AR-0M37 Breacher 6.5mm);
+        displayName = NAME(AR-0M37 Breacher 6.5mm);
         picture = QPATHTOF(data\icons\msbs65_barrel_ca.paa);
+        class XtdGearInfo {
+            model = QWEAPON(AR0M37);
+            underbarrel = "SHT";
+        };
     };
 };
