8049836ef3620b1d1882eb4f3c858f6b2e9c33dd
Freya Gaynor <freya@webwitch.org>
2023-07-05 09:24:37 -0500

    fix: Invalid EOF

--- addons/weapons/XtdGearModels.hpp
+++ addons/weapons/XtdGearModels.hpp
@@ -3,7 +3,7 @@ class XtdGearModels {
         class WEAPON(AR0M37) {
             label = NAME(AR-0M37 6.5mm);
             icon = "\sws_main\assets\icon_sws.paa";
-            options = {"Underbarrel"};
+            options[] = {"Underbarrel"};
 
             class Underbarrel {
                 label = "Underbarrel Attachment";
