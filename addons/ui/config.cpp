145110e4cf4babe8da2d5da2abb3bfbad3b7a9f3
Freya Gaynor <freya@webwitch.org>
2023-07-13 13:11:03 -0500

    fix: Maybe different load orders will make it show the splash properly

--- addons/ui/config.cpp
+++ addons/ui/config.cpp
@@ -13,7 +13,7 @@ class CfgPatches
         magazines[] = { };
         ammo[] = { };
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "SWS_Main", "SWS_FirstMeuCompat", "19th_Fleet_Armor" };
+        requiredAddons[] = { "SWS_FirstMeuCompat", "19th_Fleet_Armor" };
         author = QUOTE(AUTHOR);
         VERSION_CONFIG;
     };
