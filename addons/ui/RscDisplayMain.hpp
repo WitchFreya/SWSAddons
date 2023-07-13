ef482e3aeb70d58ea79cf9bdaad9aabff5743749
Freya Gaynor <freya@webwitch.org>
2023-07-13 12:01:04 -0500

    feature: Main menu join server
    Also fixes a model.cfg issue.

---
+++ addons/ui/RscDisplayMain.hpp
@@ -0,0 +1,37 @@
+class RscStandardDisplay;
+class RscControlsGroupNoScrollbars;
+
+class RscDisplayMain: RscStandardDisplay {
+    class controls {
+        class GroupSingleplayer: RscControlsGroupNoScrollbars {
+            class Controls;
+        };
+        class GroupMultiplayer: GroupSingleplayer {
+            h = "(4 *   1.5) *  (pixelH * pixelGrid * 2)";
+
+            class Controls: Controls {
+                class QuickPlay;
+                class Campaigns;
+                class GVAR(JoinServer): QuickPlay {
+                    idc = -1;
+                    text = "Join SWS Server (Beta)";
+                    tooltip = "If this doesn't work, use the Server Browser as normal.";
+                    y = "(1 *   1.5) *  (pixelH * pixelGrid * 2) +  (pixelH)";
+                    onButtonClick = QUOTE(connectToServer [ARR_3('chanson.sunhawk.dev',2502,'awoo')]);
+                    animTextureNormal = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                    animTextureDisabled = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                    animTextureOver = QPATHTOF(data\buttonMissionMainMenuHover_ca.paa);
+                    animTextureFocused = QPATHTOF(data\buttonMissionMainMenuHover_ca.paa);
+                    animTexturePressed = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                    animTextureDefault = QPATHTOF(data\buttonMissionMainMenu_ca.paa);
+                };
+                class ServerBrowser: Campaigns {
+                    y = "(2 *   1.5) *  (pixelH * pixelGrid * 2) +  (pixelH)";
+                };
+                class ApexProtocol: Campaigns {
+                    y = "(3 *   1.5) *  (pixelH * pixelGrid * 2) +  (pixelH)";
+                };
+            };
+        };
+    };
+};
