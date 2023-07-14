f4f9f649409b4e2d95bf1f7519ea6a94789c1abc
Freya Gaynor <freya@webwitch.org>
2023-07-14 13:40:26 -0500

    fix: No more warnings.

--- addons/patches/CfgAmmo.hpp
+++ addons/patches/CfgAmmo.hpp
@@ -1,5 +1,8 @@
 class CfgAmmo {
     class M_Scalpel_AT;
+    class OPTRE_M_Jackknife_LGM;
+
+    // 1st MEU
     class Icarus_M_Jackknife_LGM : M_Scalpel_AT {
         delete flightProfiles;
         flightProfiles[]=
@@ -9,5 +12,13 @@ class CfgAmmo {
             "TopDown"
 		};
         class Direct {};
+    };
+
+    // Optre Ordnance Expansion
+    class SAG6_Vendetta : OPTRE_M_Jackknife_LGM {
+        delete tracerColor;
+        delete tracerColorR;
+        tracerColor[] = {0.7,0.7,0.5,0.04};
+		tracerColorR[] = {0.7,0.7,0.5,0.04};
     };
 };
