7c4451a2a41a268f226e04b5308bd0fdebcaec39
Freya Gaynor <freya@webwitch.org>
2023-07-14 14:36:23 -0500

    fix: Icon placement.

--- addons/ui/RscDisplayMain.hpp
+++ addons/ui/RscDisplayMain.hpp
@@ -1,11 +1,63 @@
+class RscPicture;
 class RscStandardDisplay;
 class RscControlsGroupNoScrollbars;
+class RscActivePicture;
+class RscActiveText;
 
 class RscDisplayMain: RscStandardDisplay {
-    class controls {
+    delete RscActivePicture;
+    class RscActivePicture {
+        access = 0;
+        color[] = {1,1,1,0.5};
+        colorActive[] = {1,1,1,1};
+        colorDisabled[] = {1,1,1,0.25};
+        default = 0;
+        deleteable = 0;
+        fade = 0;
+        font = "RobotoCondensed";
+        h = 0.035;
+        idc = -1;
+        shadow = 2;
+        sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+        soundClick[] = {"",0.1,1};
+        soundEnter[] = {"",0.1,1};
+        soundEscape[] = {"",0.1,1};
+        soundPush[] = {"",0.1,1};
+        style = 48;
+        text = "";
+        tooltipColorBox[] = {1,1,1,1};
+        tooltipColorShade[] = {0,0,0,0.65};
+        tooltipColorText[] = {1,1,1,1};
+        type = 11;
+        url = "";
+        w = 0.035;
+        x = 0;
+        y = 0;
+    };
+
+    class Controls {
+        delete Spotlight1;
+		delete Spotlight2;
+		delete Spotlight3;
+		delete BackgroundSpotlightRight;
+		delete BackgroundSpotlightLeft;
+		delete BackgroundSpotlight;
+
+        class BackgroundBar;
         class GroupSingleplayer: RscControlsGroupNoScrollbars {
             class Controls;
         };
+
+        class Logo: RscActivePicture {
+            delete onButtonClick;
+            delete onSetFocus;
+            delete onKillFocus;
+            delete tooltip;
+            color[] = {1,1,1,1};
+            colorActive[] = {1,1,1,1};
+			text = QPATHTOF(data\icon-logo.paa);
+		};
+
         class GroupMultiplayer: GroupSingleplayer {
             h = "(4 *   1.5) *  (pixelH * pixelGrid * 2)";
 
@@ -32,6 +84,20 @@ class RscDisplayMain: RscStandardDisplay {
                     y = "(3 *   1.5) *  (pixelH * pixelGrid * 2) +  (pixelH)";
                 };
             };
+        };
+    };
+
+    delete Spotlight;
+    enableDisplay = 0;
+    class controlsBackground {
+        delete LoadingPic;
+        class LoadingPic : RscPicture {
+            idc=1;
+            x = "SafeZoneX";
+            y = "SafeZoneY";
+            h = "SafeZoneH";
+            w = "SafeZoneW";
+            text = QPATHTOF(data\titlescreen-drop.jpg);
         };
     };
 };
