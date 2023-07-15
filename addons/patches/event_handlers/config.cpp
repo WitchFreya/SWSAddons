ca635bff15f91232f81bad7f5cdbd02882fbda53
Freya Gaynor <freya@webwitch.org>
2023-07-15 16:30:06 -0500

    fix: TCGM divinities stuff and others
    This also puts things into a child folder

---
+++ addons/patches/event_handlers/config.cpp
@@ -0,0 +1,15 @@
+#include "CfgVehicles.hpp"
+
+class CfgPatches
+{
+    class SWS_Patches_EventHandlers
+    {
+        name = "SWS_Patches_EventHandlers";
+        units[] = { };
+        weapons[] = { };
+        magazines[] = { };
+        ammo[] = { };
+        requiredAddons[] = { };
+        addonRootClass = "SWS_Patches";
+    };
+};
