3a8fc9ec7445df4750bd0b47199a750829603f81
Freya Gaynor <freya@webwitch.org>
2023-07-16 12:04:26 -0500

    refactor: Put the pollution corrections inside of patches

---
+++ addons/patches/pollution/config.cpp
@@ -0,0 +1,17 @@
+#include "CfgAmmo.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgVehicles.hpp"
+
+class CfgPatches
+{
+    class SWS_Patches_Pollution
+    {
+        name = "SWS_Patches_Pollution";
+        units[] = { };
+        weapons[] = { };
+        magazines[] = { };
+        ammo[] = { };
+        requiredAddons[] = { };
+        addonRootClass = "SWS_Patches";
+    };
+};
