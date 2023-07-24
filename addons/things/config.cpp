3d434899afa006a22cab408c6f86050b0eaadcf1
Freya Gaynor <freya@webwitch.org>
2023-07-24 11:14:07 -0500

    refactor: Tidy up some of the stuff in the logic addon

--- addons/things/config.cpp
+++ addons/things/config.cpp
@@ -1,8 +1,5 @@
 #include "script_component.hpp"
 
-#include "ace_arsenal_stats.inc"
-#include "CfgEventHandlers.inc"
-#include "CfgFactionClasses.inc"
 #include "CfgVehicles.inc"
 
 class CfgPatches
@@ -11,12 +8,10 @@ class CfgPatches
     {
         name = QUOTE(COMPONENT);
         units[] = { CLASSES_VEHICLES };
-        weapons[] = {};
+        weapons[] = {  };
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"SWS_Main", "SWS_Gear"};
+        requiredAddons[] = {};
         author = QUOTE(AUTHOR);
         VERSION_CONFIG;
     };
 };
-
-
