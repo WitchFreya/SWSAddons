cdd53a7dd4f99868e8017b49311d2ac6d610f939
Freya Gaynor <freya@webwitch.org>
2023-07-27 15:08:01 -0500

    fix: Optics issues for BR-55

---
+++ addons/patches/19th/config.cpp
@@ -0,0 +1,17 @@
+#include "\x\SWS\addons\patches\script_component.hpp"
+
+#include "CfgWeapons.inc"
+
+class CfgPatches
+{
+    class SWS_Patches_EventHandlers
+    {
+        name = "SWS_Patches_19th";
+        units[] = { };
+        weapons[] = { };
+        magazines[] = { };
+        ammo[] = { };
+        requiredAddons[] = { "19th_Weapons_F_BattleRifle" };
+        addonRootClass = "SWS_Patches";
+    };
+};
