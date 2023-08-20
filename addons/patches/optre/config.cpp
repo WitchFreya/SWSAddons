c69cc50e23e55f8004a2fe33e93a1f52f3bdba6e
Freya Gaynor <freya@webwitch.org>
2023-08-20 13:57:23 -0500

    feature: M154 AT Mine will no longer detonate if a player walks over it.

---
+++ addons/patches/optre/config.cpp
@@ -0,0 +1,17 @@
+#include "\x\SWS\addons\patches\script_component.hpp"
+
+#include "CfgAmmo.inc"
+
+class CfgPatches
+{
+    class SWS_Patches_OPTRE
+    {
+        name = "SWS_Patches_OPTRE";
+        units[] = { };
+        weapons[] = { };
+        magazines[] = { };
+        ammo[] = { };
+        requiredAddons[] = { "OPTRE_Weapons_Charges" };
+        addonRootClass = "SWS_Patches";
+    };
+};
