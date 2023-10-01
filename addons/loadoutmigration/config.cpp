36cf1d2631bcb1a9e0d8c93443b8f3a3c5120dbb
Freya Gaynor <freya@webwitch.org>
2023-10-01 11:49:33 -0700

    fix: Typos and capitalization

---
+++ addons/loadoutmigration/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+#include "defines.hpp"
+#include "CfgEventHandlers.inc"
+#include "ace_arsenal_display.inc"
+
+class CfgPatches
+{
+    class ADDON
+    {
+        name = QUOTE(COMPONENT);
+        units[] = { };
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_arsenal", "aceax_arsenal"};
+        author = QUOTE(AUTHOR);
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/loadoutmigration/migrations/v0_1_43.hpp
@@ -0,0 +1,1 @@
+GVAR(MIGRATIONS) set ["SWS_Tech_Weapon_NVG_NeuralLace_Maid", "SWS_Tech_Weapon_NVG_NeuralLace_ONI"];
