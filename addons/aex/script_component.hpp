3cd743c45d1b32f30f4a68eef2b7586cd4910bf2
Freya Gaynor <freya@webwitch.org>
2023-10-01 14:47:43 -0700

    feature: Begin migrating AEX stuff into one addon

---
+++ addons/aex/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT aex
+#define COMPONENT_BEAUTIFIED ACE Arsenal Extended
+#include "\x\SWS\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_INGAME
+    #define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_GEAR
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_INGAME
+#endif
+
+#include "\x\SWS\addons\main\script_macros.hpp"
+
+
