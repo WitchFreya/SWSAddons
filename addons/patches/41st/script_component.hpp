650032d7c4a530e22268d15247fbc562266cb198
Freya Gaynor <freya@webwitch.org>
2023-10-08 15:37:09 -0700

    fix: Missed some casing

--- addons/patches/41st/script_component.hpp
+++ addons/patches/41st/script_component.hpp
@@ -1,3 +1,3 @@
-#define SUBCOMPONENT firesupportplus
-#define SUBCOMPONENT_BEAUTIFIED Fire Support Plus
+#define SUBCOMPONENT 41st
+#define SUBCOMPONENT_BEAUTIFIED 41st ODST/MFR - Declassified Assets
 #include "..\script_component.hpp"
