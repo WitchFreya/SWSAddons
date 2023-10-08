3ada293153745c5b7f90324dd3d3434a17fd8d8f
Freya Gaynor <freya@webwitch.org>
2023-10-08 13:43:52 -0700

    refactor: Put firesupportplus into their own mod

---
+++ addons/patches/firesupportplus/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+#include "CfgVehicles.inc"
+
+class CfgPatches
+{
+    class SUBADDON
+    {
+        name = COMPONENT_NAME;
+        units[] = { };
+        weapons[] = { };
+        magazines[] = { };
+        ammo[] = { };
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = { "CBRN_units" };
+        skipWhenMissingDependencies = 1;
+        VERSION_CONFIG;
+
+        addonRootClass = QUOTE(ADDON);
+    };
+};
