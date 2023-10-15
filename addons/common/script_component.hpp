276168b2d55fda41ed6dd78d9e49dfbcd7d1e89c
Freya Gaynor <freya@webwitch.org>
2023-10-15 14:29:47 -0700

    feature: Array reduce.

---
+++ addons/common/script_component.hpp
@@ -0,0 +1,15 @@
+#define COMPONENT common
+#define COMPONENT_BEAUTIFIED Common
+#include "\x\sws\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_INGAME
+    #define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_GEAR
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_INGAME
+#endif
+
+#include "\x\sws\addons\main\script_macros.hpp"
