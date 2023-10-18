15e1a789df8f1cae9067d80d8fb793637a5dd2e9
EidolonForgery <5998409+EidolonForgery@users.noreply.github.com>
2023-10-18 16:49:44 -0700

    Finalized supply pod init Added custom pod textures (awaiting howdy)

--- addons/resupply/config.cpp
+++ addons/resupply/config.cpp
@@ -20,12 +20,12 @@ class CfgPatches
 class CfgEditorCategories {
 	class SWSObjects
 	{
-		displayName = "[SWS] Objects"; // Vague because arma wants it to be
+		displayName = "[SWS] Objects"; 
 	};
 };
-class CfgEditorSubcateogries {
+class CfgEditorSubcategories {
     class SWSResupply {
-        displayName = "Supply Pods & Boxes"; // if you try to use "default" it moves it to "other" subcategory.
+        displayName = "Supply Pods & Boxes"; 
     };
 };
 
@@ -33,132 +33,151 @@ class CfgVehicles {
     
     class NATO_Box_Base;
 	class Box_SWS_TeamBox : NATO_Box_Base {
-        dlc = "SWS";
         displayName = "[SWS] Team Supply Box";
+        dlc = "SWS";
         author = "Black";
         icon = "iconCrateWpns";
+
+        maximumLoad = 8000;
         transportMaxWeapons = 14;
         transportMaxMagazines = 144;
-        maximumLoad = 8000;
         transportMaxBackpacks = 12;
+        
         transportAmmo = 0;
         transportRepair = 0;
         transportFuel = 0;
-        supplyRadius = 4.5;
+        supplyRadius = -1;
+
         armor = 10000;
+
         model = "\A3\Weapons_F\Ammoboxes\Supplydrop.p3d";
         editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\B_supplyCrate_F.jpg";
+
         hiddenSelections[] = {"camo"};
         hiddenSelectionsTextures[] = {"a3\weapons_f\ammoboxes\data\supplydrop_co.paa"};
-        scope = 2;
-        side = 4;
 
-        editorCategory = "SWSLogistics";
+        scope = 2;
+        scopeCurator = 2;
+        side = 3;
+
+        editorCategory = "SWSObjects";
         editorSubcategory = "SWSResupply";
 
         class TransportMagazines {
-                MAG_XX(OPTRE_60Rnd_762x51_Mag,160);
-                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs,80);
-                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs_Tracers_Blue,80);
-                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless,80);
-                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_Tracers_Blue,80);
-                MAG_XX(OPTRE_36Rnd_95x40_Mag,40);
-                MAG_XX(OPTRE_60Rnd_5x23mm_Mag,120);
-                MAG_XX(OPTRE_15Rnd_762x51_Mag,40);
+                MAG_XX(OPTRE_60Rnd_762x51_Mag,80);
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs,40);
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs_Tracers_Blue,15);
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless,40);
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_Tracers_Blue,15);
+                MAG_XX(OPTRE_36Rnd_95x40_Mag,20);
+                MAG_XX(OPTRE_60Rnd_5x23mm_Mag,30);
+                MAG_XX(OPTRE_15Rnd_762x51_Mag,20);
         };
         class TransportItems {
-            ITEM_XX(ACE_bloodIV,40);
-            ITEM_XX(ACE_elasticBandage,200);
-            ITEM_XX(ACE_quikclot,50);
+            ITEM_XX(ACE_bloodIV,30);
+            ITEM_XX(ACE_elasticBandage,100);
+            ITEM_XX(ACE_quikclot,40);
         };
         class TransportWeapons {
-            WEAP_XX(howdyclassname,12);
+            WEAP_XX(SWS_Weapon_M24RL_D_Loaded,8);
         };
     };
-
-
     class OPTRE_Ammo_SupplyPod_Empty;
     class SWS_Ammo_SupplyPod_Empty : OPTRE_Ammo_SupplyPod_Empty {
-        dlc = "OPTRE";
         displayName = "[SWS] Supply Pod (Empty)";
-
         author = "Black";
+        dlc = "SWS";
         icon = "iconCrateWpns";
-		model = "\OPTRE_misc\crates\Supply_pod.p3d";
-		hiddenselections[] = {"camo"};
-		hiddenselectionstextures[] = {"optre_misc\crates\data\supplypod_co.paa"};
+
+        scope = 2;
+        scopeCurator = 2;
+        side = 3;
+
         transportMaxWeapons = 8;
         transportMaxMagazines = 40;
-        maximumLoad = 2000;
         transportMaxBackpacks = 4;
+        maximumLoad = 4000;
+
         transportAmmo = 0;
         transportRepair = 0;
         transportFuel = 0;
-        supplyRadius = 1.5;
-        armor = 10000;
-        scope = 2;
-        side = 4;
 
-        editorCategory = "SWSLogistics";
-        editorSubcategory = "SWSResupply";
+        supplyRadius = -1;
+        armor = 10000;
+        
+        model = "\OPTRE_misc\crates\Supply_pod.p3d";
+
+		hiddenselections[] = {"camo"};
+		hiddenselectionstextures[] = {QPATHTOF(data\sws_supplypod_base_co.paa)};
+
+        OBJ_CATEGORY(SWSResupply);
         class TransportMagazines{};
         class TransportWeapons{};
         class TransportItems{};
     };
     class SWS_Ammo_SupplyPod_Basic : SWS_Ammo_SupplyPod_Empty {
-        DLC = "SWS";
         displayName = "[SWS] Basic Resupply";
         author = "Black";
-        hiddenselectionstextures[] = {"optre_misc\crates\data\OPTRE_MA5k_Pod.paa"};
-        scope = 2;
-        side = 4;
-
-        editorCategory = "SWSLogistics";
-        editorSubcategory = "SWSResupply";
+        hiddenselectionstextures[] = {QPATHTOF(data\sws_supplypod_ammo_co.paa)};
         class TransportMagazines
             {
-                MAG_XX(OPTRE_60Rnd_762x51_Mag,40);
+                MAG_XX(OPTRE_60Rnd_762x51_Mag,30);
                 MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs,10);
-                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs_Tracers_Blue,10);
-                MAG_XX(OPTRE_36Rnd_95x40_Mag,10);
-                MAG_XX(OPTRE_60Rnd_5x23mm_Mag,15);
-                MAG_XX(OPTRE_15Rnd_762x51_Mag,10);
-        };
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_msbs_Tracers_Blue,5);
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless,10);
+                MAG_XX(SWS_Magazine_60Rnd_65x39_Caseless_Tracers_Blue,5);
+                MAG_XX(OPTRE_36Rnd_95x40_Mag,8);
+                MAG_XX(OPTRE_60Rnd_5x23mm_Mag,10);
+                MAG_XX(OPTRE_15Rnd_762x51_Mag,8);
+            };
     };
-
     class SWS_Ammo_SupplyPod_Medical : SWS_Ammo_SupplyPod_Empty {
         displayName = "[SWS] Medical Resupply";
-        DLC = "SWS";
         author = "Black";
-        hiddenselectionstextures[] = {"optre_misc\crates\data\OPTRE_MA5k_Pod.paa"};
-        scope = 2;
-        side = 4;
-
-        editorCategory = "SWSLogistics";
-        editorSubcategory = "SWSResupply";
+        hiddenselectionstextures[] = {QPATHTOF(data\sws_supplypod_medical_co.paa)};
         class TransportItems {
             ITEM_XX(ACE_bloodIV,10);
             ITEM_XX(ACE_elasticBandage,40);
             ITEM_XX(ACE_quikclot,10);
         };
     };
-
     class SWS_Ammo_SupplyPod_Howdy : SWS_Ammo_SupplyPod_Empty {
         displayName = "[SWS] Howdy Resupply";
-        DLC = "SWS";
         author = "Black";
-        hiddenselectionstextures[] = {"optre_misc\crates\data\OPTRE_MA5k_Pod.paa"};
-        scope = 2;
-        side = 4;
-
-        editorCategory = "SWSLogistics";
-        editorSubcategory = "SWSResupply";
+        hiddenselectionstextures[] = {QPATHTOF(data\sws_supplypod_base_co.paa)};
         class TransportWeapons
         {
-            WEAP_XX(howdyclassname,4);
+            WEAP_XX(SWS_Weapon_M24RL_D_Loaded,4);
         };
-        
-    };    
+    };
+    class SWS_Ammo_SupplyPod_Spnkr : SWS_Ammo_SupplyPod_Empty {
+        displayName = "[SWS] SPNKR Resupply";
+        author = "Black";
+        hiddenselectionstextures[] = {QPATHTOF(data\sws_supplypod_spnkr_co.paa)};
+        class TransportWeapons
+            {
+            WEAP_XX(OPTRE_M41_SSR,1);
+            };
+        class TransportMagazines
+            {
+                MAG_XX(OPTRE_M41_Twin_HEAT_Thermal_ProximityFuse,2);
+                MAG_XX(OPTRE_M41_Twin_HEAT_SACLOS,4);
+                MAG_XX(OPTRE_M41_Twin_HEAT_SALH,2);
+                MAG_XX(OPTRE_M41_Twin_HEAT_Guided,2);
+                MAG_XX(OPTRE_M41_Twin_HEAP,2);
+                MAG_XX(OPTRE_M41_Twin_HEAT,2);
+            };
+    };
+    class SWS_Ammo_SupplyPod_SM247 : SWS_Ammo_SupplyPod_Empty {
+        displayName = "[SWS] SM247 Resupply";
+        author = "Black";
+        hiddenselectionstextures[] = {QPATHTOF(data\sws_supplypod_mg_co.paa)};
+        class TransportMagazines
+            {
+                MAG_XX(SWS_Magazine_200Rnd_762x51_Mag,4);
+                MAG_XX(SWS_Magazine_200Rnd_762x51_Mag_Tracer,4);
+                MAG_XX(SWS_Magazine_400Rnd_762x51_Mag,2);
+                MAG_XX(SWS_Magazine_400Rnd_762x51_Mag_Tracer,2);
+            };
+    };        
 };
-
-
--- addons/resupply/script_component.hpp
+++ addons/resupply/script_component.hpp
@@ -13,16 +13,11 @@
 
 #include "\x\sws\addons\main\script_macros.hpp"
 
-#define POD_MIDDLE Ammo_SupplyPod
-#define POD_EDITOR_DETAILS(var1,var2,var3) editorCategory = QUOTE(var1); \
-                    editorSubcategory = QUOTE(var2); \
-                    side = 4;\
-                    SCOPE(var3)
 
-
+#define OBJ_CATEGORY(var1) editorCategory = QUOTE(SWSObjects); editorSubcategory = QUOTE(var1)
 
 /// Equipment list macros definition ///
-#define POD_XX(var0) class TRIPLES(PREFIX,POD_MIDDLE,var0)
+
 #define MAG_XX(var0,var1) class _xx_##var0 { magazine = QUOTE(var0); count = var1; }
 #define WEAP_XX(var0,var1) class _xx_##var0 { weapon = QUOTE(var0); count = var1; }
 #define ITEM_XX(var0,var1) class _xx_##var0 { name = QUOTE(var0); count = var1; }
