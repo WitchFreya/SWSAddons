a5408ff5c31a58da649488827c7f71e7782c01a6
Freya Gaynor <freya@webwitch.org>
2023-10-19 16:35:39 -0700

    feature: remove role progression for its own mod

--- addons/roleprogression/defines.hpp
+++
@@ -1,8 +0,0 @@
-#include "\z\ace\addons\arsenal\defines.hpp"
-
-// Custom IDCs
-#define IDC_iconBackgroundRole 2038
-#define IDC_buttonRole 2039
-
-#define IDX_virtMisc 17 // The virtual items in the miscellaneous category
-
