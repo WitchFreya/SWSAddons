305c268c464600082e993958cf952b1f2f535f53
Freya Gaynor <freya@webwitch.org>
2023-10-28 12:06:03 -0700

    refactor: Put demining drone in air

---
+++ addons/air/uav_demining/script_component.hpp
@@ -0,0 +1,3 @@
+#define SUBCOMPONENT uav_demining
+#define SUBCOMPONENT_BEAUTIFIED Demining Drone
+#include "..\script_component.hpp"
--- addons/air/uav_smq94/script_component.hpp
+++ addons/air/uav_smq94/script_component.hpp
@@ -1,3 +1,3 @@
-#define SUBCOMPONENT smq94
+#define SUBCOMPONENT uav_smq94
 #define SUBCOMPONENT_BEAUTIFIED SMQ-94 Recon Drone
 #include "..\script_component.hpp"
