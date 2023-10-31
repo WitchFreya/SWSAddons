919a98197aba1b151ec062dfcfec6409730c0b0d
Emily Winjum <38087295+EmilyWinjum@users.noreply.github.com>
2023-10-31 16:37:47 -0700

    add viper sticker variant

--- addons/vehicles/config.cpp
+++ addons/vehicles/config.cpp
@@ -11,12 +11,12 @@ class CfgPatches {
         ammo[] = { };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
-            "A3_Armor_F_EPB"
-            , "ace_medical"
-            , "OPTRE_UNSC_Units_Army"
-            , "sws_common"
-            , "sws_main"
-            , "sws_weapons"
+            "A3_Armor_F_EPB",
+            "ace_medical",
+            "OPTRE_UNSC_Units_Army",
+            "sws_common",
+            "sws_main",
+            "sws_weapons"
         };
         author = QUOTE(AUTHOR);
         VERSION_CONFIG;
