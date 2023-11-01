25f60698fb5c36f692a5b599db1c1d09183e0321
Emily Winjum <38087295+EmilyWinjum@users.noreply.github.com>
2023-11-01 10:59:26 -0700

    feat: viper atgm rebalance

--- addons/vehicles/config.cpp
+++ addons/vehicles/config.cpp
@@ -1,17 +1,29 @@
 #include "script_component.hpp"
 
+class SWS_SensorTargetTemplate
+{
+    minRange = 8000;
+    maxRange = 8000;
+    objectDistanceLimitCoef = -1;
+    viewDistanceLimitCoef = -1;
+};
+
+#include "CfgAmmo.inc"
+#include "CfgMagazines.inc"
 #include "CfgVehicles.inc"
+#include "CfgWeapons.inc"
 
 class CfgPatches {
     class ADDON {
         name = QUOTE(COMPONENT);
         units[] = { CLASSES_VEHICLES };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
+        weapons[] = { CLASSES_WEAPONS };
+        magazines[] = { CLASSES_MAGAZINES };
+        ammo[] = { CLASSES_AMMO };
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "A3_Armor_F_EPB",
+            "A3_Weapons_F_Tank",
             "ace_medical",
             "OPTRE_UNSC_Units_Army",
             "sws_common",
