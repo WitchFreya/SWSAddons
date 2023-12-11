729d0059e40b735d7bc1f750fa0d9958b57867ed
Aisha <5998409+EidolonForgery@users.noreply.github.com>
2023-12-11 08:26:13 -0800

    feat(resupply): Add/Update Pods (#13)
    - Add Sabotage Pod: Mines
    - Add Demolition Pod: Explosives
    - Add Engineer Pod: Support Items
    - All supplies now carryable/draggable
    
    ---------
    
    Co-authored-by: Freya Gaynor <freya@webwitch.org>

--- addons/resupply/script_component.hpp
+++ addons/resupply/script_component.hpp
@@ -22,9 +22,3 @@
 
 
 #define OBJ_CATEGORY(var1) editorCategory = QUOTE(SWSObjects); editorSubcategory = QUOTE(var1)
-
-/// Equipment list macros definition ///
-
-#define MAG_XX(var0,var1) class _xx_##var0 { magazine = QUOTE(var0); count = var1; }
-#define WEAP_XX(var0,var1) class _xx_##var0 { weapon = QUOTE(var0); count = var1; }
-#define ITEM_XX(var0,var1) class _xx_##var0 { name = QUOTE(var0); count = var1; }
