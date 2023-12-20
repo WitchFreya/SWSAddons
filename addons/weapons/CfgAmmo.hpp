86b6fa436f89e73ec56fc63510c979fc141ff1d9
Freya Gaynor <freya@webwitch.org>
2023-12-20 14:34:59 -0800

    feat(weapons): Remove 9Bangs and M9Sx throwables

--- addons/weapons/CfgAmmo.hpp
+++ addons/weapons/CfgAmmo.hpp
@@ -1,20 +1,13 @@
 #define CLASSES_AMMO \
     "SWS_B_762x51_Ball", \
     "SWS_B_Grenade_Frag", \
-    "SWS_B_Grenade_M9Sx", \
     "SWS_B_Grenade_M3Sx", \
     "SWS_Magazine_Grenade_M3Sx_40mm_Shell", \
-    "SWS_B_Grenade_M9Sx_OnImpact", \
-    "SWS_B_M9Sx_OnExp", \
     "SWS_B_Grenade_M9Sx_40mm_Shell", \
-    "SWS_B_Grenade_9Bang", \
-    "SWS_B_Grenade_9Bang_NoFuse", \
-    "SWS_Magazine_Grenade_9Bang_Shell", \
     "SWS_B_C7Sx_Sticky", \
     "SWS_B_C7Sx_Sticky_Explosion", \
     "SWS_B_8Gauge_Incendiary", \
     "SWS_B_8Gauge_Incendiary_Submunition"
-
 
 class CfgAmmo {
     //== Bullets
@@ -26,14 +19,12 @@ class CfgAmmo {
 
     //== Grenades & Shells
     class OPTRE_G_M9_Frag;
-    class ACE_G_CTS9;
     class G_40mm_HE;
     class M319_HE;
     class M319_HEDP;
 
     #include "CfgAmmo\M3Sx.inc"
     #include "CfgAmmo\M9Sx.inc"
-    #include "CfgAmmo\9Bang.inc"
 
     //== Explosives
     class OPTRE_c7_remote_ammo_thrown_sticky;
