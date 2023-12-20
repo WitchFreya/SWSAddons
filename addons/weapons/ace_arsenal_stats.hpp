0acd5790ffdff6038b6b73b4b82bcc39a7d6f458
Freya Gaynor <freya@webwitch.org>
2023-12-20 15:40:31 -0800

    refactor(weapons): Remove unused file

--- addons/weapons/ace_arsenal_stats.hpp
+++
@@ -1,7 +0,0 @@
-class ACE_Arsenal_Stats {
-    class StatBase;
-
-    class ACE_explosionTime : StatBase {
-        condition = QUOTE(!(configName (_this select 1)) in OPTRE_STICKYABLE_NADE_TYPES_MAP);
-    };
-};
