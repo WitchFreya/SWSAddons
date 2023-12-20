eef1280a8885c3b04bac159630242f61abbb0aad
Freya Gaynor <freya@webwitch.org>
2023-12-20 11:49:22 -0800

    fix(scifisupportplus): Extend from the correct class for mags

--- addons/patches/scifisupportplus/config.cpp
+++ addons/patches/scifisupportplus/config.cpp
@@ -1,21 +1,21 @@
 #include "script_component.hpp"
 
 #include "CfgAmmo.inc"
+#include "CfgMagazines.inc"
 
 class CfgPatches
 {
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { CLASSES_AMMO };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "OPAEX_AirCore", "OPAEX_Glassing", "PHAN_OPAEX" };
-        skipWhenMissingDependencies = 1;
-        VERSION_CONFIG;
-
-        addonRootClass = QUOTE(ADDON);
-    };
+  class SUBADDON
+  {
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { CLASSES_AMMO };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { "OPAEX_AirCore", "OPAEX_Glassing", "PHAN_OPAEX" };
+    skipWhenMissingDependencies = 1;
+    VERSION_CONFIG;
+    addonRootClass = QUOTE(ADDON);
+  };
 };
