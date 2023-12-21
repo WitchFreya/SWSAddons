b2704e4312dc1263177e9a3da0e266c3a72ca272
Freya Gaynor <freya@webwitch.org>
2023-12-20 20:38:10 -0800

    feat(things): Toolkit counts as entrenching tool

--- addons/things/config.cpp
+++ addons/things/config.cpp
@@ -1,21 +1,22 @@
 #include "script_component.hpp"
 
+#include "CfgWeapons.inc"
 #include "CfgVehicles.inc"
 
 class CfgPatches
 {
-    class ADDON
-    {
-        name = QUOTE(COMPONENT);
-        units[] = {
-            #ifdef CLASSES_VEHICLES
-            CLASSES_VEHICLES
-            #endif
-        };
-        weapons[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {};
-        author = QUOTE(AUTHOR);
-        VERSION_CONFIG;
+  class ADDON
+  {
+    name = QUOTE(COMPONENT);
+    units[] = {
+        #ifdef CLASSES_VEHICLES
+        CLASSES_VEHICLES
+        #endif
     };
+    weapons[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {};
+    author = QUOTE(AUTHOR);
+    VERSION_CONFIG;
+  };
 };
