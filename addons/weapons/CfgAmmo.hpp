03ed6f7173c86213967342e1bbbf0bbe307b0abe
Freya Gaynor <freya@webwitch.org>
2023-12-20 16:00:59 -0800

    refactor(weapons): Put 65 ammo buff into CfgAmmo direct

--- addons/weapons/CfgAmmo.hpp
+++ addons/weapons/CfgAmmo.hpp
@@ -1,19 +1,23 @@
 #define CLASSES_AMMO \
-    "SWS_B_762x51_Ball", \
-    "SWS_B_8Gauge_Incendiary", \
-    "SWS_B_8Gauge_Incendiary_Submunition"
+  "SWS_B_762x51_Ball", \
+  "SWS_B_8Gauge_Incendiary", \
+  "SWS_B_8Gauge_Incendiary_Submunition"
 
 class CfgAmmo {
-    //== Bullets
-    class OPTRE_B_762x51_Ball;
-    class BulletBase;
+  //== Bullets
+  class OPTRE_B_762x51_Ball;
+  class BulletBase;
 
-    #include "CfgAmmo\762x51.inc"
-    #include "CfgAmmo\65x39_Caseless.inc"
+  class B_65x39_Caseless : BulletBase
+  {
+    hit = 18;
+  };
 
-    //== Shotgun
-    class B_12Gauge_Pellets_Submunition;
-    class B_12Gauge_Pellets_Submunition_Deploy;
+  #include "CfgAmmo\762x51.inc"
 
-    #include "CfgAmmo\Incendiary.inc"
+  //== Shotgun
+  class B_12Gauge_Pellets_Submunition;
+  class B_12Gauge_Pellets_Submunition_Deploy;
+
+  #include "CfgAmmo\Incendiary.inc"
 };
