f144ced396fee6c6e8d1ca654147d1a6e984751e
Freya Gaynor <freya@webwitch.org>
2023-12-21 14:20:19 -0800

    feat(weapons/ma5bsx): Move to subcomponent and add recon sight to UGL

---
+++ addons/weapons/ma5bsx/config.cpp
@@ -0,0 +1,27 @@
+#include "script_component.hpp"
+
+class Single;
+class FullAuto;
+class WeaponSlotsInfo;
+class PointerSlot;
+class CowsSlot;
+class SWS_Scopes_762mm;
+class EGLM;
+
+#include "CfgWeapons.inc"
+
+class CfgPatches
+{
+  class SUBADDON
+  {
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { CLASSES_WEAPONS };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"OPTRE_Weapons_AR"};
+    VERSION_CONFIG;
+    addonRootClass = QUOTE(ADDON);
+  };
+};
