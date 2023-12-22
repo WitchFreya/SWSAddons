399d75dbcccaeea43a9400c5e8224024955d9cc2
Freya Gaynor <freya@webwitch.org>
2023-12-21 18:18:03 -0800

    fix(opaex): No dumb event handlers for you.

--- addons/patches/optrearsenalexpansion/config.cpp
+++ addons/patches/optrearsenalexpansion/config.cpp
@@ -1,20 +1,21 @@
 #include "script_component.hpp"
+
 #include "CfgVehicles.inc"
+#include "EventHandlers.inc"
 
 class CfgPatches
 {
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "OPTRE_Vehicles_Air", "OPAEX_Hornet" };
-        skipWhenMissingDependencies = 1;
-        VERSION_CONFIG;
-
-        addonRootClass = QUOTE(ADDON);
-    };
+  class SUBADDON
+  {
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { "OPTRE_Vehicles_Air", "OPAEX_Hornet", "OPAEX_D20", "OPAEX_Functions" };
+    skipWhenMissingDependencies = 1;
+    VERSION_CONFIG;
+    addonRootClass = QUOTE(ADDON);
+  };
 };
