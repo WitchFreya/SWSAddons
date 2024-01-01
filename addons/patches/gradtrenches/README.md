4d41832c0775c1c2504559ac2a31e06f6f5fbee8
Freya Gaynor <freya@webwitch.org>
2024-01-01 14:00:14 -0800

    fix(patches): Avoid overwriting final functions

---
+++ addons/patches/gradtrenches/README.md
@@ -0,0 +1,3 @@
+# GRAD
+
+This removes CfgDigVehicles, thus not allowing the PostInit adding of event handlers that GRAD does. Since they don't use CBA properly, there's no other way to remove those elegantly.
--- addons/patches/gradtrenches/config.cpp
+++ addons/patches/gradtrenches/config.cpp
@@ -1,18 +1,18 @@
 #include "script_component.hpp"
 
-#include "CfgFunctions.inc"
+delete CfgDigVehicles;
 
-class CfgPatches {
-    class SUBADDON {
-        name = QUOTE(ADDON);
-        units[] = {};
-        weapons[] = {};
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "grad_trenches_functions" };
-        skipWhenMissingDependencies = 1;
-        VERSION_CONFIG;
-
-        addonRootClass = QUOTE(ADDON);
-    };
+class CfgPatches
+{
+  class SUBADDON
+  {
+    name = QUOTE(ADDON);
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { "grad_trenches_functions" };
+    skipWhenMissingDependencies = 1;
+    VERSION_CONFIG;
+    addonRootClass = QUOTE(ADDON);
+  };
 };
-
