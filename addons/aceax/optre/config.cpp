4d156ce324b345bffefc8d7f4332946ce7594518
Freya Gaynor <freya@webwitch.org>
2024-01-01 15:20:52 -0800

    refactor(aceax): Tidy up the format on the optre subcomponent

--- addons/aceax/optre/config.cpp
+++ addons/aceax/optre/config.cpp
@@ -5,18 +5,17 @@
 
 class CfgPatches
 {
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { "OPTRE_Weapons" };
-        skipWhenMissingDependencies = 1;
-        VERSION_CONFIG;
-
-        addonRootClass = QUOTE(ADDON);
-    };
+  class SUBADDON
+  {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { "OPTRE_Weapons" };
+    skipWhenMissingDependencies = 1;
+    VERSION_CONFIG;
+  };
 };
