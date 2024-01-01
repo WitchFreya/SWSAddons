80fbcc18c4a32d8a8b6cf5a77a850b439bf9e284
Freya Gaynor <freya@webwitch.org>
2024-01-01 15:05:53 -0800

    fix(cbrn_scripts): Stop throwing an error on respawn

---
+++ addons/cbrn_scripts/README.md
@@ -0,0 +1,5 @@
+# CBRN Scripts
+
+Someday CBA will have macros for function definition within subcomponents but I don't wanna go through the work of doing that myself.
+
+If they do add it, we can put this in the `patches` folder.
---
+++ addons/cbrn_scripts/script_component.hpp
@@ -0,0 +1,15 @@
+#define COMPONENT cbrn_scripts
+#define COMPONENT_BEAUTIFIED CBRN Scripts Patches for Fire Support Plus
+#include "\x\sws\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#ifdef DEBUG_ENABLED_INGAME
+    #define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_GEAR
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_INGAME
+#endif
+
+#include "\x\sws\addons\main\script_macros.hpp"
