e9e3ab8e5ff6603d8ce7e49c4d4aaa90bf6367f4
Freya Gaynor <freya@webwitch.org>
2024-01-01 16:44:27 -0800

    refactor(air): Tidy up format

--- addons/air/uav_demining/config.cpp
+++ addons/air/uav_demining/config.cpp
@@ -1,18 +1,19 @@
 #include "script_component.hpp"
+
 #include "CfgVehicles.inc"
 
 class CfgPatches
 {
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { CLASSES_VEHICLES };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3_Air_F_Orange_UAV_06"};
-        VERSION_CONFIG;
-        addonRootClass = QUOTE(ADDON);
-    };
+  class SUBADDON
+  {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { CLASSES_VEHICLES };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"A3_Air_F_Orange_UAV_06"};
+    VERSION_CONFIG;
+  };
 };
