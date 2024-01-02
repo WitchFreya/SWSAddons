1970678ff940e41fd165026209d93a46016e6184
Freya Gaynor <freya@webwitch.org>
2024-01-01 16:56:19 -0800

    refactor(modules): Put respawn into subcomponent

--- addons/modules/config.cpp
+++ addons/modules/config.cpp
@@ -1,17 +1,16 @@
 #include "script_component.hpp"
 
 #include "CfgEventHandlers.inc"
-#include "CfgVehicles.inc"
 
 class CfgPatches
 {
   class ADDON
   {
     name = QUOTE(COMPONENT);
-    units[] = { CLASSES_VEHICLES };
+    units[] = { };
     weapons[] = {};
     requiredVersion = REQUIRED_VERSION;
-    requiredAddons[] = { "A3_Modules_F_Multiplayer", "sws_common", "sws_logic" };
+    requiredAddons[] = { "sws_common", "sws_logic" };
     author = QUOTE(AUTHOR);
     VERSION_CONFIG;
   };
