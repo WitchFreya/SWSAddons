6cfd93e6a5e02da759b4b760caa4cdf0242c3803
Freya Gaynor <freya@webwitch.org>
2024-01-04 08:38:41 -0800

    refactor(cba): Remove EH override for A3 items
    CBA added first-class support for A3 2.14 items

--- addons/patches/config.cpp
+++ addons/patches/config.cpp
@@ -1,19 +1,17 @@
 #include "script_component.hpp"
 
-#include "CfgVehicles.inc"
-
 class CfgPatches
 {
-    class ADDON
-    {
-        name = QUOTE(COMPONENT);
-        units[] = { };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { };
-        author = QUOTE(AUTHOR);
-        VERSION_CONFIG;
-    };
+  class ADDON
+  {
+    name = QUOTE(COMPONENT);
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { };
+    author = QUOTE(AUTHOR);
+    VERSION_CONFIG;
+  };
 };
