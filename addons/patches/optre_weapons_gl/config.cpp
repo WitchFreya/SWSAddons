2a0210941c74617fe0b56e858f8fb94cb41f51eb
Freya Gaynor <freya@webwitch.org>
2024-01-13 13:02:44 -0800

    fix(#58): Allow sidearm GL to use 3rnds (#63)
    * fix(#58): Add 3-round GLs to OPTRE primary/secondary GL
    * feat(weapons): Add our stuff to CBA magwells
    * refactor(weapons): Use append for magazineWell

---
+++ addons/patches/optre_weapons_gl/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+
+#include "CfgWeapons.inc"
+
+class CfgPatches
+{
+  class SUBADDON
+  {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"OPTRE_Weapons_GL"};
+    skipWhenMissingDependencies = 1;
+    VERSION_CONFIG;
+  };
+};
