1f5df08b3ce010df115d3845f2b4331c59f133f5
Freya Gaynor <freya@webwitch.org>
2024-01-18 10:49:04 -0800

    fix(retirement): Patch sound config warning from dropbays

---
+++ addons/patches/retirement/config.cpp
@@ -0,0 +1,20 @@
+#include "script_component.hpp"
+
+#include "CfgVehicles.inc"
+
+class CfgPatches
+{
+  class SUBADDON
+  {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"Retirement"};
+    skipWhenMissingDependencies = 1;
+    VERSION_CONFIG;
+  };
+};
