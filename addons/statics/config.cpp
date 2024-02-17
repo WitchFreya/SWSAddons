b3fa17693e3070e88cba6da03f113399287a41d1
Freya Gaynor <freya@webwitch.org>
2024-02-16 19:01:22 -0800

    feat(statics): OPTRE statics can be destroyed with a custom effect (#140)

---
+++ addons/statics/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+#include "OPTRE_DestructionEffectsTemplate.inc"
+#include "CfgVehicles.inc"
+#include "CfgEventHandlers.inc"
+
+class CfgPatches {
+  class ADDON {
+    name = QUOTE(COMPONENT);
+    units[] = {};
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { "OPTRE_Weapons_StaticTurret", "sws_main", "sws_common"};
+    author = QUOTE(AUTHOR);
+    VERSION_CONFIG;
+  };
+};
