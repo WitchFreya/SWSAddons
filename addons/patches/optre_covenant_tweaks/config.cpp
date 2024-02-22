13cdd8840993f420f3d5c86a8d77535f97b9d984
Freya Gaynor <freya@webwitch.org>
2024-02-22 09:19:57 -0800

    chore(patches/optre_covenant_tweaks): Formatting and inheritance

--- addons/patches/optre_covenant_tweaks/config.cpp
+++ addons/patches/optre_covenant_tweaks/config.cpp
@@ -5,20 +5,23 @@ class MainTurret;
 
 #include "CfgVehicles.inc"
 
-class CfgPatches
-{
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"OPTRE_FC_Weapons_Tyrant", "OPTRE_FC_Weapons_Shrike", "OPTRE_FC_Weapons_Shade", "OPTRE_FC_Weapons_Draugr"};
-        skipWhenMissingDependencies = TRUE;
-        VERSION_CONFIG;
-        addonRootClass = QUOTE(ADDON);
+class CfgPatches {
+  class SUBADDON {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {
+      "OPTRE_FC_Weapons_Tyrant"
+      , "OPTRE_FC_Weapons_Shrike"
+      , "OPTRE_FC_Weapons_Shade"
+      , "OPTRE_FC_Weapons_Draugr"
     };
+    skipWhenMissingDependencies = TRUE;
+    VERSION_CONFIG;
+  };
 };
 
