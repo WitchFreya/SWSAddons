2ee07e435f951526f295ab6b8a32a3301f2dbce4
Aisha <5998409+EidolonForgery@users.noreply.github.com>
2024-02-29 08:48:00 -0800

    feat(songbird): Shrike helo (#150)
    ---------
    
    Co-authored-by: Freya Gaynor <freya@webwitch.org>

---
+++ addons/air/heli_songbird/config.cpp
@@ -0,0 +1,42 @@
+#include "script_component.hpp"
+
+
+class Components;
+class TransportPylonsComponent;
+class Turrets;
+class CopilotTurret;
+class LowROF;
+class Burst1;
+class Burst2;
+class AnimationSources;
+#include "CfgVehicles.inc"
+#include "CfgWeapons.inc"
+#include "CfgAmmo.inc"
+#include "CfgMagazines.inc"
+
+class CfgPatches
+{
+  class SUBADDON
+  {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { CLASSES_VEHICLES };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {
+      "MELB"
+      , "a3_weapons_f"
+      , "b_unscaf"
+      , "optre_weapons_ammo_signal_smoke"
+      , "optre_weapons_smg"
+      , "ace_hellfire"
+      , "ace_medical_treatment"
+      , "sws_main"
+      , "sws_weapons_ar0m37s"
+      };
+    skipWhenMissingDependencies = TRUE;
+    VERSION_CONFIG;
+  };
+};
