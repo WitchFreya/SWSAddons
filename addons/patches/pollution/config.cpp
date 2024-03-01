d8ca7b27f1a6c80fe6a44ba5658483ae3484ba53
Freya Gaynor <freya@webwitch.org>
2024-02-29 20:07:36 -0800

    fix(pollution): New and exciting OPTRE pollution

--- addons/patches/pollution/config.cpp
+++ addons/patches/pollution/config.cpp
@@ -3,19 +3,16 @@
 #include "CfgWeapons.inc"
 #include "CfgVehicles.inc"
 
-class CfgPatches
-{
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { CLASSES_VEHICLES };
-        weapons[] = { CLASSSES_WEAPONS };
-        magazines[] = { };
-        ammo[] = { CLASSES_AMMO };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = { };
-        VERSION_CONFIG;
-
-        addonRootClass = QUOTE(ADDON);
-    };
+class CfgPatches {
+  class SUBADDON {
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { CLASSSES_WEAPONS };
+    magazines[] = { };
+    ammo[] = { CLASSES_AMMO };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = { };
+    VERSION_CONFIG;
+    addonRootClass = QUOTE(ADDON);
+  };
 };
