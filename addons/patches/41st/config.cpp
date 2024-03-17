c37abad9587538ff0409e41b705377a4f7d000cc
Freya Gaynor <freya@webwitch.org>
2024-03-17 18:52:36 -0500

    fix(41): Allow stomper RCWS to work again (#169)

--- addons/patches/41st/config.cpp
+++ addons/patches/41st/config.cpp
@@ -1,24 +1,35 @@
 #include "script_component.hpp"
+
+class CBA_Extended_EventHandlers_Base;
+class EventHandlers;
+class Turrets;
+class MainTurret;
+class NewTurret;
+class Components;
+class RCWSOptics;
+class DefaultVehicleSystemsDisplayManagerLeft;
+class DefaultVehicleSystemsDisplayManagerRight;
+class CargoTurret_01;
+
 #include "CfgVehicles.inc"
 
-class CfgPatches
-{
-    class SUBADDON
-    {
-        name = COMPONENT_NAME;
-        units[] = { };
-        weapons[] = { };
-        magazines[] = { };
-        ammo[] = { };
-        requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {
-            "Retirement",
-            "MyAddon", // I wish I was joking, they actually called it that.
-            "41ST_ODST_FACTIONS",
-            "OPTRE_Vehicles_Pelican"};
-        skipWhenMissingDependencies = 1;
-        VERSION_CONFIG;
-
-        addonRootClass = QUOTE(ADDON);
-    };
+class CfgPatches {
+  class SUBADDON {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    units[] = { };
+    weapons[] = { };
+    magazines[] = { };
+    ammo[] = { };
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {
+        "Retirement"
+        , "MyAddon" // I wish I was joking, they actually called it that.
+        , "41ST_ODST_FACTIONS"
+        , "OPTRE_Vehicles_Pelican"
+        , "41_Hogosha"
+      };
+    skipWhenMissingDependencies = TRUE;
+    VERSION_CONFIG;
+  };
 };
