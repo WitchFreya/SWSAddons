a57f9211b88fe860b69a9788021ad80e5f7237d3
Freya Gaynor <freya@webwitch.org>
2024-03-24 11:56:25 -0700

    feat(smq_94): Reference MELB properly

--- addons/air/uav_smq94/script_component.hpp
+++ addons/air/uav_smq94/script_component.hpp
@@ -1,3 +1,6 @@
 #define SUBCOMPONENT uav_smq94
 #define SUBCOMPONENT_BEAUTIFIED SMQ-94 Recon Drone
 #include "..\script_component.hpp"
+
+#define PATHTO_MELB(varPath) melb\##varPath
+#define QPATHTO_MELB(varPath)  QUOTE(PATHTO_MELB(varPath))
