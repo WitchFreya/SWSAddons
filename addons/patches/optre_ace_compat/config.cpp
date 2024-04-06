0c586803a1408773aff873f7aa172d6e4ef3e179
Freya Gaynor <freya@webwitch.org>
2024-04-06 10:24:51 -0700

    feature(weapons): Cleanup flashlight code and ACEAX configs (#177)
    * fix(optre_ace_compat): Remove flashlight from MA37K
    
    * fix(optre_act_compat): Remove flashlight from MA5B
    
    * refactor(weapons): Clean up some of the formatting and aceax

---
+++ addons/patches/optre_ace_compat/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+#include "CfgWeapons.inc"
+
+class CfgPatches {
+  class SUBADDON {
+      addonRootClass = QUOTE(ADDON);
+      name = COMPONENT_NAME;
+      units[] = { };
+      weapons[] = { };
+      magazines[] = { };
+      ammo[] = { };
+      requiredVersion = REQUIRED_VERSION;
+      requiredAddons[] = {
+        "OPTRE_Weapons"
+        , "OPTRE_ACE_Compat"
+      };
+      skipWhenMissingDependencies = TRUE;
+      VERSION_CONFIG;
+  };
+};
