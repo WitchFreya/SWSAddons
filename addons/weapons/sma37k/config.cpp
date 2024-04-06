2985d6d7a99cc97a02f5e9068c5381f6f755a32d
Freya Gaynor <freya@webwitch.org>
2024-04-06 10:22:43 -0700

    refactor(weapons): Clean up some of the formatting and aceax

--- addons/weapons/sma37k/config.cpp
+++ addons/weapons/sma37k/config.cpp
@@ -2,10 +2,8 @@
 
 #include "CfgWeapons.inc"
 
-class CfgPatches
-{
-  class SUBADDON
-  {
+class CfgPatches {
+  class SUBADDON {
     addonRootClass = QUOTE(ADDON);
     name = COMPONENT_NAME;
     units[] = { };
@@ -14,7 +12,6 @@ class CfgPatches
     ammo[] = { };
     requiredVersion = REQUIRED_VERSION;
     requiredAddons[] = {"OPTRE_Weapons_MA37K"};
-    skipWhenMissingDependencies = 1;
     VERSION_CONFIG;
   };
 };
