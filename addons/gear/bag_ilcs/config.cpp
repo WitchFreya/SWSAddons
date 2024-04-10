47f8031d6912d8e91aff1c535c3c8b06d5dcc312
Freya Gaynor <freya@webwitch.org>
2024-04-09 20:42:57 -0700

    fix(gear/bag_ilcs): Deps

--- addons/gear/bag_ilcs/config.cpp
+++ addons/gear/bag_ilcs/config.cpp
@@ -12,10 +12,9 @@ class CfgPatches {
     magazines[] = { };
     requiredVersion = REQUIRED_VERSION;
     requiredAddons[] = {
-      "OPTRE_Weapons_Backpacks"
+      "OPTRE_Weapons"
       , "OPTRE_ACE_Compat"
       , "sws_main"
-      , "sws_aceax"
     };
     VERSION_CONFIG;
   };
