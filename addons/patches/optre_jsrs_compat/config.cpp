5b00ce94224940e65322ae6c2ebc29a2cfebd3c0
Freya Gaynor <freya@webwitch.org>
2024-04-23 13:46:39 -0700

    fix(optre): Correct the inheritance for the JSRS compat mod (#194)

---
+++ addons/patches/optre_jsrs_compat/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class StandardSound;
+class Single;
+
+#include "CfgWeapons.inc"
+
+class CfgPatches {
+  class SUBADDON {
+    addonRootClass = QUOTE(ADDON);
+    name = COMPONENT_NAME;
+    requiredVersion = REQUIRED_VERSION;
+    requiredAddons[] = {"OPTRE_JSRS_Sounds"};
+    skipWhenMissingDependencies = TRUE;
+    VERSION_CONFIG;
+  };
+};
---
+++ addons/patches/optre_jsrs_compat/script_component.hpp
@@ -0,0 +1,3 @@
+#define SUBCOMPONENT optre_jsrs_compat
+#define SUBCOMPONENT_BEAUTIFIED Operation: TREBUCHET - JSRS Compatability Mod
+#include "..\script_component.hpp"
