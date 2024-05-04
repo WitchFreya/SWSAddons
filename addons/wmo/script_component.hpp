a66630a7c5965b71785a34fcaff12691442ddf3a
Freya Gaynor <freya@webwitch.org>
2024-05-04 15:25:25 -0700

    feat(wmo): Prevent stamina scamming on vehicle (#217)

--- addons/wmo/script_component.hpp
+++ addons/wmo/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT wmo
-#define COMPONENT_BEAUTIFIED Walkable Moving Objects Rework
+#define COMPONENT_BEAUTIFIED Walkable Moving Objects Patches
 #include "\x\sws\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
@@ -13,13 +13,6 @@
 #endif
 
 #include "\x\sws\addons\main\script_macros.hpp"
-
-#define CF_ONLY_WATER 1
-#define CF_NEAREST_CONTACT 2
-#define CF_ONLY_STATIC 4
-#define CF_ONLY_DYNAMIC 8
-#define CF_FIRST_CONTACT 16
-#define CF_ALL_OBJECTS  32
 
 #ifdef DEBUG_MODE_FULL
   #define ROADWAY_OBJECT "BW_roadway_obj_debug"
