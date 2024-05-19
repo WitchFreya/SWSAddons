ec6690367a3b8641a5618d8b9978449dc325217e
Freya Gaynor <freya@webwitch.org>
2024-05-19 10:33:25 -0700

    feat(gear): Move Manny's bag with the other rucksacks and add variants (#231)

--- addons/gear/bag_rucksack/config.cpp
+++ addons/gear/bag_rucksack/config.cpp
@@ -1,5 +1,7 @@
 #include "script_component.hpp"
 
+class XtdGearInfo;
+
 #include "CfgVehicles.inc"
 
 class CfgPatches {
--- addons/gear/bag_rucksack/script_component.hpp
+++ addons/gear/bag_rucksack/script_component.hpp
@@ -1,3 +1,7 @@
 #define SUBCOMPONENT bag_rucksack
-#define SUBCOMPONENT_BEAUTIFIED Rucksack
+#define SUBCOMPONENT_BEAUTIFIED ILCS Rucksack
 #include "..\script_component.hpp"
+
+#define HS_VAR_BIOFOAM "biofoam"
+#define HS_VAR_SOFTPACKS "AP_Heavy"
+#define HS_BASE "camo1", HS_VAR_SOFTPACKS, HS_VAR_BIOFOAM
